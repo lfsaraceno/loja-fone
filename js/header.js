a94aeafa46faa80389bec7d72da004f364320363
Fernando Saraceno <lfsaraceno@hotmail.com>
2019-09-16 20:17:10 -0300

    A18 add menu hamburger moble version

--- js/header.js
+++ js/header.js
@@ -1,10 +1,19 @@
 const $search = document.querySelector(".header-action");
 const $icon = $search.querySelector(".icon");
 const $active = $search.querySelector(".active");
+const $menu = document.querySelector(".menu");
+const $nav = document.querySelector(".navigation-store");
 
 $icon.addEventListener("click", event => {
   event.preventDefault();
 
   $search.classList.toggle("active");
   $icon.classList.toggle("-close");
+});
+
+$menu.addEventListener("click", event => {
+  event.preventDefault();
+
+  $menu.classList.toggle("-close");
+  $nav.classList.toggle("-open");
 });
